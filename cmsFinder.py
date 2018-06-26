e5bfab60e8536fa378a13ddabf30e6ec0b74455b
williamspiro <35824747+williamspiro@users.noreply.github.com>
2018-06-26 19:06:23 -0400

    error exceptions

--- cmsFinder.py
+++ cmsFinder.py
@@ -1,14 +1,21 @@
 from bs4 import BeautifulSoup
 import urllib2
+from urllib2 import URLError, HTTPError
 
-pagesToScrub = ["http://www.puppy.com", "http://cats.com", "http://www.kittens.com"]
+pagesToScrub = ["http://advantagedistributors.com", "http://abidonetwork.com"]
 
 for pageToScrub in pagesToScrub:
-    page = urllib2.urlopen(pageToScrub)
-    soup = BeautifulSoup(page, "html.parser")
-    if soup.find(attrs={"name":"generator"}):
-        cmsMeta = soup.find(attrs={"name":"generator"})
-        cms = cmsMeta.get("content")
-        print "{} uses the cms {}".format(pageToScrub, cms)
+    try:
+        page = urllib2.urlopen(pageToScrub)
+    except HTTPError as e:
+        print "hmmm, I had a hard time fetching {}".format(pageToScrub)
+    except URLError as e:
+        print "hmmm, I had a hard time fetching {}".format(pageToScrub)
     else:
-        print "hmmm, I was not able to figure out which cms {} uses".format(pageToScrub)
+        soup = BeautifulSoup(page, "html.parser")
+        if soup.find(attrs={"name":"generator"}):
+            cmsMeta = soup.find(attrs={"name":"generator"})
+            cms = cmsMeta.get("content")
+            print "{} uses the cms {}".format(pageToScrub, cms)
+        else:
+            print "hmmm, I was not able to figure out which cms {} uses".format(pageToScrub)
