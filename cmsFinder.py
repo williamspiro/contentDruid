7631c3921029f5712575f4d69992dc847bcc87ad
williamspiro <35824747+williamspiro@users.noreply.github.com>
2018-06-28 12:12:05 -0400

    Fix cmsTextMatch

--- cmsFinder.py
+++ cmsFinder.py
@@ -5,6 +5,7 @@ import requests
 import json
 
 pagesToScrub = ["http://www.puppy.com", "http://cats.com", "http://www.kittens.com"]
+
 psiBaseApi = (r"https://www.googleapis.com/pagespeedonline/v4/runPagespeed?url=http%3A%2F%2F")
 print ("Domain, CMS, Desktop PSI Score, Mobile PSI Score")
 
@@ -16,6 +17,22 @@ def psiGrade (gradeUri):
         return (psiSpeed)
     else:
         return ("unknown")
+
+def cmsTextMatch (html):
+    if b"static.wixstatic.com" in html or b"wix" in html:
+        return ("Wix")
+    elif b"wp-content" in html or b"wordpress" in html or b"wp-json" in html: 
+        return ("Wordpress")
+    elif b"squarespace" in html: # or b"squarespace" in html
+        return ("Squarespace")
+    elif b"weebly" in html:
+        return ("Weebly")
+    elif b"drupal" in html:
+        return ("Drupal")
+    elif b"craftCMS" in html or b"CRAFT" in html:
+        return ("CRAFT")
+    else:
+        return ("unknown")    
 
 for pageToScrub in pagesToScrub:
     cms = ""
@@ -33,21 +50,8 @@ for pageToScrub in pagesToScrub:
             cmsMeta = soup.find(attrs={"name":"generator"})
             cms = cmsMeta.get("content")
         else:
-            html = page.read()
-            if b"wp-content" or b"wordpress" or b"wp-json" in html:
-                cms = ("Wordpress")
-            elif b"static.wixstatic.com" or b"wix" in html:
-                cms = ("Wix")
-            elif b"static1.squarespace.com" or b"squarespace" in html:
-                cms = ("Squarespace")
-            elif b"weebly" in html:
-                cms = ("Weebly")
-            elif b"drupal" in html:
-                cms = ("Drupal")
-            elif b"craftCMS" or b"CRAFT" in html:
-                cms = ("CRAFT")
-            else:
-                cms = ("unknown")
+            cms = cmsTextMatch(urlopen(pageToScrub).read())
+            
     psiGradableUri = pageToScrub.split("//")[1]
     psiDeskReqUri = (f"{psiBaseApi}{psiGradableUri}&fields=id%2CruleGroups")
     psiMobReqUri = (f"{psiBaseApi}{psiGradableUri}&fields=id%2CruleGroups&strategy=mobile")
