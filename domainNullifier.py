1ba410e18c9e2ec300cd96d9b411f33035bc5f47
William Spiro <35824747+williamspiro@users.noreply.github.com>
2018-09-13 12:39:09 -0400

    Domain nullifier

---
+++ domainNullifier.py
@@ -0,0 +1,26 @@
+from time import sleep
+import requests
+import json
+import sys
+
+accessToken = sys.argv[1]
+domain = sys.argv[2]
+apiBase = (f"https://api.hubapi.com/cospages/v1/pages/list/{domain}")
+pagesSlothApiQuery = (f"access_token={accessToken}&limit=1000&property=id&property=domain")
+
+slothedPages = requests.get(apiBase, params=pagesSlothApiQuery)
+slothedPageObjects = slothedPages.json()["objects"]
+
+for slothedPageObject in slothedPageObjects:
+    slothedPageId = slothedPageObject["id"]
+    slothedPageDomain = slothedPageObject["domain"]
+    if slothedPageDomain == domain:
+        print (f"Page ID {slothedPageId} has a specified domain key of {domain}, attempting to clear now")
+        pageDomainNullifyUri = (f"https://api.hubspot.com/cospages/v1/landing-pages/{slothedPageId}?access_token={accessToken}")
+        pageDomainNullifyPayload = {'domain': ''}
+        pageDomainNullifyPage = requests.put(pageDomainNullifyUri, json=pageDomainNullifyPayload)
+        if pageDomainNullifyPage.status_code == 200:
+            print (f"Nullified domain of {slothedPageId}")
+        else:
+            print (f"Failed to nullify domain of {slothedPageId}")
+        sleep(.33)
