dc402e77f315ce51e6c9cf28e320aa425f5c7b9e
William Spiro <35824747+williamspiro@users.noreply.github.com>
2018-09-25 18:58:51 -0400

    tags restore

---
+++ tagsSlothRestore.py
@@ -0,0 +1,33 @@
+from time import sleep
+import requests
+import json
+import sys
+
+accessToken = sys.argv[1]
+slothStart = int(sys.argv[2])
+tagsApiBase = "https://api.hubapi.com/blogs/v3/topics"
+tagsSlothApiQuery = (f"access_token={accessToken}&includeDeleted=true&limit=1000")
+
+slothedTags = requests.get(tagsApiBase, params=tagsSlothApiQuery)
+slothedTagObjects = slothedTags.json()["objects"]
+
+for slothedTagObject in slothedTagObjects:
+    slothedTagId = slothedTagObject["id"]
+    slothedTagSlug = slothedTagObject["slug"]
+    slothedTagDeletedAt = slothedTagObject["deletedAt"]
+    if slothedTagDeletedAt >= slothStart:
+        tagsApiRestore = requests.put(f"{tagsApiBase}/{slothedTagId}/restore-deleted?access_token={accessToken}")
+        if tagsApiRestore.status_code == 200:
+            if "#DEL" in slothedTagSlug:
+                tagRevisedSlug = slothedTagSlug.split("#DEL")[0]
+                tagSlugUpdatePayload = {"slug":tagRevisedSlug} 
+                tagSlugUpdatePayloadRequest = requests.put(f"{tagsApiBase}/{slothedTagId}?access_token={accessToken}", json=tagSlugUpdatePayload)
+                if tagsApiRestore.status_code == 200:
+                    print (f"Restored tag {slothedTagId} and removed #DEL suffix")
+                else:
+                    print (f"Restored tag {slothedTagId} but failed to remove #DEL suffix")
+            else:
+                print (f"Restored tag {slothedTagId}")
+        else:
+            print (f"Hmmm, something wen wrong resoring tag id {slothedTagId}")
+        sleep(.33)
