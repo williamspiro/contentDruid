9b5b0ab342d655c8d1a536182bcf1ddcb8239b62
William Spiro <35824747+williamspiro@users.noreply.github.com>
2018-11-19 17:46:17 -0500

    Only publish based on stateWhenDeleted

--- postsSlothRestore.py
+++ postsSlothRestore.py
@@ -7,7 +7,7 @@ accessToken = sys.argv[1]
 slothStart = sys.argv[2]
 contentGroupId = sys.argv[3]
 postsApiBase = "https://api.hubapi.com/blogs/v3/blog-posts"
-postsSlothApiQuery = (f"access_token={accessToken}&include_deleted=true&deleted_at__gt={slothStart}&limit=1000&contentGroupId={contentGroupId}")
+postsSlothApiQuery = (f"access_token={accessToken}&include_deleted=true&deleted_at__gt={slothStart}&limit=1000&contentGroupId={contentGroupId}&property=stateWhenDeleted&property=id")
 
 slothedPosts = requests.get(postsApiBase, params=postsSlothApiQuery)
 slothedPostObjects = slothedPosts.json()["objects"]
@@ -16,16 +16,20 @@ print (f"Hold your butts! Restoring {deletedPostsIdsCount} posts :butt-holdings:
 
 for slothedPostObject in slothedPostObjects:
     slothedPostId = slothedPostObject["id"]
+    slothedPostStateWhenDeleted = slothedPostObject["stateWhenDeleted"]
     postsApiRestore = (f"{postsApiBase}/{slothedPostId}/restore-deleted?access_token={accessToken}")
     restorePost = requests.put(postsApiRestore)
     if restorePost.status_code == 200:
-        postRestoreRequestUri = (f"{postsApiBase}/{slothedPostId}/publish-action?access_token={accessToken}")
-        publishPostPayload = {'action': 'schedule-publish'}
-        publishedPost = requests.post(postRestoreRequestUri, json=publishPostPayload)
-        if publishedPost.status_code == 204:
-            print (f"Restored and published post id {slothedPostId}")
+        if slothedPostStateWhenDeleted == "PUBLISHED":
+            postRestoreRequestUri = (f"{postsApiBase}/{slothedPostId}/publish-action?access_token={accessToken}")
+            publishPostPayload = {'action': 'schedule-publish'}
+            publishedPost = requests.post(postRestoreRequestUri, json=publishPostPayload)
+            if publishedPost.status_code == 204:
+                print (f"Restored and published post id {slothedPostId}")
+            else:
+                print (f"Failed to publish post id {slothedPostId}, but it was restored")
         else:
-            print (f"Failed to publish post id {slothedPostId}, but it was restored")
+            print (f"Restored post id {slothedPostId}, and left it in a draft state")
     else:
         print (f"Hmmm, something went wrong resoring post id {slothedPostId}")
     sleep(.33)
